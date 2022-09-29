0008c0098483ada528ec1f253de06a3d555dd0d4
mauriciolomba <mauriciolomba@hotmail.com>
2022-09-29 17:13:56 -0300

    First Notification Example

---
+++ app/src/main/java/com/mauriciolomba/androidnotificationexample/AfterNotification.kt
@@ -0,0 +1,11 @@
+package com.mauriciolomba.androidnotificationexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AfterNotification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after_notification)
+    }
+}
